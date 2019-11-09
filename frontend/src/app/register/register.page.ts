9a17ef8238f5109084308ddb66d211d25d6e5591
Amel <hamidovic@student.tugraz.at>
2019-11-09 17:03:45 +0100

    added RegisterPage

---
+++ frontend/src/app/register/register.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { catchError } from 'rxjs/operators';
+import { throwError } from '../../../node_modules/rxjs';
+import {ApiException, RegisterVm, UserClient, UserVm} from '../../app/app.api';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+})
+export class RegisterPage implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private userClient: UserClient,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      registrationCode: ['', [Validators.required, Validators.minLength(6)]],
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      firstName: '',
+      lastName: '',
+    });
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.displayValidationErrors();
+      return;
+    }
+
+    const registerVm: RegisterVm = new RegisterVm(this.form.value);
+    this.userClient.register(registerVm)
+        .pipe(catchError((err: ApiException) => throwError(err)))
+        .subscribe((user: UserVm) => {
+          console.log(user);
+          this.router.navigate(['/login']);
+        }, (err: ApiException) => {
+          console.log(err);
+        });
+  }
+
+  private displayValidationErrors() {
+    const formKeys = Object.keys(this.form.controls);
+    formKeys.forEach(key => {
+      this.form.controls[key].markAsDirty();
+      this.form.controls[key].updateValueAndValidity();
+    });
+  }
+
+}
