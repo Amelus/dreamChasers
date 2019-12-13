b14a281a94d6f75e3c58130bdbdbac3a066ec979
Amel <hamidovic@student.tugraz.at>
2019-12-13 12:11:33 +0100

    added calender and styling .. somehow

--- frontend/src/app/home/home.page.ts
+++ frontend/src/app/home/home.page.ts
@@ -1,43 +1,79 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {MenuController} from '@ionic/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import bootstrapPlugin from '@fullcalendar/bootstrap';
+import {UserClient, UserVmRole} from '../app.api';
+import {FullCalendarComponent} from '@fullcalendar/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
+export class HomePage implements OnInit, AfterViewInit {
 
-  calendarPlugins = [dayGridPlugin, timeGridPlugin, interactionPlugin];
-  calendarEvents = [
-    { title: 'event 1', date: '2019-12-15' },
-    { title: 'event 2', date: '2019-12-18' }
-  ];
+    @ViewChild('calendarComponent', {static: false}) calendarComponent: FullCalendarComponent;
 
-  constructor(public menuController: MenuController) {
-    this.menuController.enable(true, 'mainMenu');
-  }
+    editorUser: boolean;
+    calendarPlugins = [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin, bootstrapPlugin];
+    calendarEvents = [
+        {title: 'event 1', date: '2019-12-15'},
+        {title: 'event 2', date: '2019-12-18'}
+    ];
+    selectedEvent: any;
 
-  addEvent() {
-    this.calendarEvents = this.calendarEvents.concat({ title: 'event 3', date: '2019-12-22' });
-  }
+    constructor(public menuController: MenuController,
+                private userClient: UserClient) {
+        this.menuController.enable(true, 'mainMenu');
+    }
 
-   modifyTitle(eventIndex, newTitle) {
-    const calendarEvents = this.calendarEvents.slice(); // a clone
-    const singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
-    singleEvent.title = newTitle;
-    calendarEvents[eventIndex] = singleEvent;
-    this.calendarEvents = calendarEvents; // reassign the array
-  }
+    ngOnInit(): void {
+    }
 
-  handleDateClick($event: any) {
-    console.log('click');
-    alert($event.dateStr);
-  }
+    ngAfterViewInit(): void {
+        this.editorUser = this.isEditorUser();
+    }
 
-  ngOnInit(): void {
-  }
+    addEvent() {
+        this.calendarEvents = this.calendarEvents.concat({title: 'event 3', date: '2019-12-22'});
+    }
+
+    modifyTitle(eventIndex, newTitle) {
+        const calendarEvents = this.calendarEvents.slice(); // a clone
+        const singleEvent = Object.assign({}, calendarEvents[eventIndex]); // a clone
+        singleEvent.title = newTitle;
+        calendarEvents[eventIndex] = singleEvent;
+        this.calendarEvents = calendarEvents; // reassign the array
+    }
+
+    handleDateClick($event: any) {
+        console.log('click');
+        alert($event.dateStr);
+    }
+
+
+    addAppointmentModal() {
+        // trigger modal
+    }
+
+    private isEditorUser(): boolean {
+        if (!this.userClient.getSessionUser()
+            || this.userClient.getSessionUser() === undefined
+            || this.userClient.getSessionUser() === null) {
+            return false;
+        }
+
+        return !(this.userClient.getSessionUser().role === undefined
+            || this.userClient.getSessionUser().role === null
+            || this.userClient.getSessionUser().role === UserVmRole.User);
+
+    }
+
+    showDateClick($event: any) {
+        const calendarApi = this.calendarComponent.getApi();
+        calendarApi.gotoDate($event.date);
+    }
 }
